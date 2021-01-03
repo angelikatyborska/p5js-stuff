1ce7f78c977f85150bfc9b1ce5de61c441f180e9
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-01-03 15:36:34 +0100

    Implement rule 30

--- index.js
+++ index.js
@@ -1,17 +1,82 @@
 const p5 = require('p5');
 
+function decimalTo8BitBinaryAsArray(quotient, acc = []) {
+  if (quotient === 0) {
+    const missingLeadingZeros = 8 - acc.length;
+    if (missingLeadingZeros > 0) {
+      for (let i = 0; i < missingLeadingZeros; i++) {
+        acc.unshift(0)
+      }
+    }
+
+    return acc;
+  } else {
+    const remainder = quotient % 2;
+    const newQuotient = Math.floor(quotient / 2);
+    const newAcc = [remainder, ...acc]
+    return decimalTo8BitBinaryAsArray(newQuotient, newAcc);
+  }
+}
+
 const sketch = (s) => {
+  const squareSize = 10;
+  const rowLength = 200;
+  const columnLength = 100;
+  const width = rowLength * squareSize;
+  const height = columnLength * squareSize;
+  const center = Math.floor(rowLength / 2);
+  let startingRow = [...(Array(rowLength)).keys()]
+  startingRow = startingRow.map((n, i) => i === center ? 1 : 0)
+
+
+  const rule = 30;
+  const reversedRule = decimalTo8BitBinaryAsArray(rule).reverse();
+
+  const grid = [startingRow];
+
+  const calculateRow = (oldRow) => {
+    return oldRow.map((n, i, a) => {
+      let neighborhoodIndices;
+
+      if (i === 0) {
+        neighborhoodIndices = [a.length - 1, i, i + 1]
+      } else if (i === a.length - 1) {
+        neighborhoodIndices = [i - 1, i, 0]
+      } else {
+        neighborhoodIndices = [i - 1, i, i + 1]
+      }
+
+      const neighborhood = neighborhoodIndices.map(i => a[i])
+      const newStateIndex = neighborhood.reverse().reduce((acc, neighbor, j) => {
+        return acc + Math.pow(2, j) * neighbor
+      }, 0)
+
+      return reversedRule[newStateIndex];
+    })
+  }
+
+  for (let i = 1; i < columnLength; i++) {
+    grid[i] = calculateRow(grid[i - 1]);
+  }
+
   s.setup = () => {
-    s.createCanvas(800, 800);
+    s.createCanvas(width, height);
+    s.noLoop();
   }
 
   s.draw = () => {
-    if (s.mouseIsPressed) {
-      s.fill(0);
-    } else {
-      s.fill(255);
-    }
-    s.ellipse(s.mouseX, s.mouseY, 80, 80);
+    s.noStroke();
+    grid.forEach((row, rowNumber) => {
+      row.forEach((n, columnNumber) => {
+        if (n === 1) {
+          s.fill(0);
+        } else {
+          s.fill(255);
+        }
+
+        s.rect(columnNumber * squareSize, rowNumber * squareSize, squareSize, squareSize);
+      })
+    })
   }
 }
 
